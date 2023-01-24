cea7e154bf95e73d4e3e24a746d5a89477040616
chloro30 <chloro30@gmail.com>
2023-01-24 15:25:26 +0900

    완성.

--- src/components/Boards.js
+++ src/components/Boards.js
@@ -1,46 +1,80 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
 import "./Boards.css"
 
-export default class Boards extends Component {
+const Boards = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
     }
+    return null;
+  }
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = 'Winner: ' + winner;
+  }else {
+    status = `Next player ${xIsNext ? 'X' : 'O'}`
+  }
+  
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice(); // 배열 얕은 복사
+
+    if(calculateWinner(newSquares) || newSquares[i]){
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares)
+    // setXIsNext(!xIsNext);
+    setXIsNext(prev => !prev); //매개변수에 state 값을 가져와서 사용할 수 있다.
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice(); // 배열 얕은 복사
-    squares[i] = 'X';
-    this.setState({squares: squares});
-  }
-
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={()=>handleClick(i)}/>
   }     
 
-  render() {
-    return (
-      <div>
-        <div className='status'>Next Player: X, O</div>
-        <div className='board-row'>
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-        </div>
-        <div className='board-row'>
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-        </div>
+  
+  return (
+    <div>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
       </div>
-    )
-  }
+      <div className='board-row'>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+      </div>
+    </div>
+  )
 }
+
+export default Boards;
